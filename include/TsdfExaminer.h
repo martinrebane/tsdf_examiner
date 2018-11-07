520fe3895b6f22f7a260195ac840eeebf298cc25
Martin Rebane <martin@linuxator.com>
2018-11-07 21:02:34 +0000

    Save 2 consecutive .ply meshes

--- include/TsdfExaminer.h
+++ include/TsdfExaminer.h
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <voxblox_ros/tsdf_server.h>
 #include <string>
+#include <stdio.h>
 
 #ifndef TSDFEXAMINER
 #define TSDFEXAMINER
@@ -12,12 +13,13 @@ public:
 	TsdfExaminer(const ros::NodeHandle& nh,
 				const ros::NodeHandle& nh_private);
 	virtual ~TsdfExaminer();
-	void initialDebug();
+	void initialDebug(int interation);
 
 private:
 	ros::NodeHandle nh_;
 	ros::NodeHandle nh_private_;
 	voxblox::TsdfServer tsdf_server_;
+	std::string mesh_filename_;
 };
 
 } /* namespace tsdf_examiner */
--- src/TsdfExaminer.cpp
+++ src/TsdfExaminer.cpp
@@ -8,15 +8,14 @@ TsdfExaminer::TsdfExaminer(const ros::NodeHandle& nh,
 			nh_private_(nh_private),
 			tsdf_server_(nh_, nh_private_)
 				{
-	// TODO Auto-generated constructor stub
-
+	nh_private.getParam("mesh_filename", mesh_filename_);
 }
 
 TsdfExaminer::~TsdfExaminer() {
 	// TODO Auto-generated destructor stub
 }
 
-void TsdfExaminer::initialDebug() {
+void TsdfExaminer::initialDebug(int iteration) {
 	ROS_INFO("DEBUG STARTED");
 	// lae salvestatud TSDF
 	//tsdf_server_.loadMap("/home/martin/jtmp/map/oxpath.vxblx");
@@ -25,9 +24,12 @@ void TsdfExaminer::initialDebug() {
 	//tsdf_server_.mesh_filename_ = "/home/martin/jtmp/map/saved_mesh.ply";
 	// wait for output to be started
 	//ros::Duration(12.0).sleep();
+
 	try {
 		tsdf_server_.updateMesh();
 		tsdf_server_.generateMesh();
+		// rename .ply file to include iteration number
+		rename(mesh_filename_.c_str(), (mesh_filename_ + std::to_string(iteration) + ".ply").c_str());
 	} catch (std::exception& ex){
 		ROS_ERROR("Something wrong with generating mesh! %s", ex.what());
 	}
@@ -51,8 +53,8 @@ int main(int argc, char** argv) {
 	while(ros::ok()) {
 		ros::spinOnce();
 		rate.sleep();
-		if (i == 100) {
-			examiner.initialDebug();
+		if (i == 100 || i == 101) {
+			examiner.initialDebug(i);
 		}
 		ROS_ERROR("Line: %d", i);
 		i++;
